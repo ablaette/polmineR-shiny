3490335f363af7bf596d1eae8a6080fe270dc335
Andreas Blaette <andreas.blaette@uni-due.de>
2014-09-20 20:55:58 +0200

    first commit

---
+++ R/polmineR-shiny-package.R
@@ -0,0 +1,11 @@
+#' Package 'polmineR-shiny'
+#' 
+#' A plugin for the polmineR-package
+#' 
+#' @author Andreas Blaette (andreas.blaette@@uni-due.de)
+#' @references http://polmine.sowi.uni-due.de
+#' @keywords package
+#' @docType package
+#' @rdname polmineR-shiny-package
+#' @name polmineR-shiny-package
+NULL
